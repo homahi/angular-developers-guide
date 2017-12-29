3bf38797dd5ac870ebbeb2a844cf2c529df2002a
homahi <homahi0128@gmail.com>
2017-12-29 17:30:20 +0900

    2-1-8:双方向データバインディング
    ngModuleを用いるには、FormsModuleのimportsが必要である.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ClassBindingComponent } from './class-binding/class-binding.component';
 import { StyleBindingComponent } from './style-binding/style-binding.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ClassBindingComponent,
-    StyleBindingComponent
+    StyleBindingComponent,
+    TwoWayComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
