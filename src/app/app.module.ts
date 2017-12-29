530df912e4a9cc0393a7d61345e13e35ad1cbf2b
homahi <homahi0128@gmail.com>
2017-12-29 17:10:49 +0900

    2-1-6:クラスバインディング

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ClassBindingComponent } from './class-binding/class-binding.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClassBindingComponent
   ],
   imports: [
     BrowserModule
