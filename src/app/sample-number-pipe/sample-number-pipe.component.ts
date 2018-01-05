b1c32d30584c67f73ef8897049f872815385934a
homahi <homahi0128@gmail.com>
2018-01-05 14:07:14 +0900

    6-1-3:lowercase/uppercase

--- src/app/sample-number-pipe/sample-number-pipe.component.ts
+++ src/app/sample-number-pipe/sample-number-pipe.component.ts
@@ -9,11 +9,13 @@ export class SampleNumberPipeComponent implements OnInit {
   pi: number = 3.141;
   e: number = 2.718281828459045;
   public euro = 0;
+  public str: string = ';'
 
   constructor() { }
 
   ngOnInit() {
     this.euro = 100000.12;
+    this.str = 'aNgUlAr';
   }
 
 }
